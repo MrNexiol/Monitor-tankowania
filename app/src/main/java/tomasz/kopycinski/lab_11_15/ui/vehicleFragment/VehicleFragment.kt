b2beea959c28bbfe3f5d35b480136a0d81c7f6f9
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-08-19 16:02:02 +0200

    Added actionBar menu to vehicle fragment

--- app/src/main/java/tomasz/kopycinski/lab_11_15/ui/vehicleFragment/VehicleFragment.kt
+++ app/src/main/java/tomasz/kopycinski/lab_11_15/ui/vehicleFragment/VehicleFragment.kt
@@ -1,10 +1,9 @@
 package tomasz.kopycinski.lab_11_15.ui.vehicleFragment
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import tomasz.kopycinski.lab_11_15.R
 import tomasz.kopycinski.lab_11_15.databinding.FragmentVehicleBinding
 
@@ -12,8 +11,27 @@ class VehicleFragment : Fragment() {
     private var _binding: FragmentVehicleBinding? = null
     private val binding get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentVehicleBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.vehicle_list_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_add -> {
+                Toast.makeText(context, "Działa", Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
