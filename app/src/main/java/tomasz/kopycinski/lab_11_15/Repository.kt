be74e6d8b188754302e6f1423ff0c7fd0865a241
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-08-19 15:06:29 +0200

    Added getVehicles method to repository Changed flow to live data

--- app/src/main/java/tomasz/kopycinski/lab_11_15/Repository.kt
+++ app/src/main/java/tomasz/kopycinski/lab_11_15/Repository.kt
@@ -1,6 +1,11 @@
 package tomasz.kopycinski.lab_11_15
 
+import androidx.lifecycle.LiveData
 import tomasz.kopycinski.lab_11_15.persistence.dao.VehicleDAO
+import tomasz.kopycinski.lab_11_15.persistence.entity.Vehicle
 
 class Repository(private val vehicleDAO: VehicleDAO) {
+    fun getVehicles(): LiveData<List<Vehicle>> {
+        return vehicleDAO.getAll()
+    }
 }
