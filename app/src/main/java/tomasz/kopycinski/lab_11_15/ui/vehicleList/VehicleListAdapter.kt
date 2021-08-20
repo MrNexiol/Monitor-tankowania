1f7d9e6aa16d2593bef85ce560634cb8768379ee
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-08-20 15:07:02 +0200

    Added recycler view to vehicle list fragment

---
+++ app/src/main/java/tomasz/kopycinski/lab_11_15/ui/vehicleList/VehicleListAdapter.kt
@@ -0,0 +1,35 @@
+package tomasz.kopycinski.lab_11_15.ui.vehicleList
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import tomasz.kopycinski.lab_11_15.databinding.VehicleRecyclerItemBinding
+import tomasz.kopycinski.lab_11_15.persistence.entity.Vehicle
+
+class VehicleListAdapter : RecyclerView.Adapter<VehicleListAdapter.ViewHolder>() {
+    private var data: List<Vehicle> = listOf()
+
+    class ViewHolder(
+        val binding: VehicleRecyclerItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = VehicleRecyclerItemBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.vehicleBrand.text = data[position].brand
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setData(data: List<Vehicle>) {
+        this.data = data
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/tomasz/kopycinski/lab_11_15/ui/vehicleList/VehicleListFragment.kt
+++ app/src/main/java/tomasz/kopycinski/lab_11_15/ui/vehicleList/VehicleListFragment.kt
@@ -3,13 +3,18 @@ package tomasz.kopycinski.lab_11_15.ui.vehicleList
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import tomasz.kopycinski.lab_11_15.R
 import tomasz.kopycinski.lab_11_15.databinding.FragmentVehicleBinding
 
 class VehicleListFragment : Fragment() {
     private var _binding: FragmentVehicleBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: VehicleListFragmentViewModel by viewModels()
+    private val adapter = VehicleListAdapter()
+    private val layoutManager = LinearLayoutManager(context)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,17 @@ class VehicleListFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentVehicleBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.vehiclesRecycler.adapter = adapter
+        binding.vehiclesRecycler.layoutManager = layoutManager
+
+        viewModel.vehicleList.observe(viewLifecycleOwner, {
+            adapter.setData(it)
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
